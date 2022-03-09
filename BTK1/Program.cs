5e387ad79a29d24678fb72a80542ec25e6da9794
iskyldrm <isakyildirim@outlook.com>
2022-03-09 14:07:02 +0300

    Add project files.

---
+++ BTK1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BTK1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
