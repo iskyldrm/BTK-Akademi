7f0b3a3ba140b20912e7b32e3cc538edd65696fe
isak yıldırım <isakyildirim@outlook.com>
2022-04-27 14:05:57 +0300

    Eventlar

---
+++ BTK7-Events/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTK7_Events
+{
+    public delegate void StockControl();
+    public class Product
+    {
+        public event StockControl StockControlEvent;
+    }
+}
